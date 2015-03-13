6525d1bfcb29245d4dd1ef83fd5741b168ba9cbe
Daniel Jänecke <daniel.jaenecke@sedo.com>
2015-03-13 21:03:16 +0100

    mainly cosmetic changes

--- clickr.js
+++ clickr.js
@@ -2,25 +2,69 @@
  * Created by daniel on 13.03.15.
  */
 
-
+/**
+ *
+ * @constructor
+ */
 Clickr = function () {
 
+    /**
+     * the id of the element holding the current image
+     * @type {string}
+     */
     this.ELEMENT_IMAGE = "clickr_image";
+
+    /**
+     * the id of the overlay element
+     * @type {string}
+     */
     this.ELEMENT_OVERLAY = "overlay";
 
-    this.image_width = null;
-    this.image_height = null;
+    /**
+     * controls whether this.debug() will write to console.log
+     * or simply will do nothing at all
+     * @type {boolean}
+     */
+    this.DEBUG = true;
 
+    /**
+     * holds the tiles covering the current image
+     * @type {Array}
+     */
     this.tiles = [];
 
+    /**
+     * the image directory
+     * @type {String}
+     */
     this.imageDir = null;
+
+    /**
+     * an array of images
+     * @type {Array} of img
+     */
     this.images = [];
 
+    /**
+     * the tile directory
+     * @type {String}
+     */
     this.tileDir = null;
+
+    /**
+     * an array of tiles
+     * @type {Array}
+     */
     this.tileImages = [];
 
 }
 
+/**
+ * get the absolute position of an element
+ *
+ * @param elem
+ * @returns {*}
+ */
 Clickr.prototype.getPosition = function (elem) {
     if (!elem) {
         return {"x": 0, "y": 0};
@@ -33,13 +77,27 @@ Clickr.prototype.getPosition = function (elem) {
     return xy;
 }
 
-
+/**
+ * write a log message to console.log
+ * @param msg
+ * @returns {Clickr}
+ */
 Clickr.prototype.debug = function (msg) {
-    console.log(msg);
+    if (this.DEBUG) {
+        console.log(msg);
+    }
     return this;
 }
 
-Clickr.prototype.adjustOverlay = function(image) {
+/**
+ * adjust and fill the overlay
+ *
+ * will position, scale and fill the overlay div to hide the current image
+ *
+ * @param image
+ * @returns {Clickr}
+ */
+Clickr.prototype.adjustOverlay = function (image) {
 
     this.elementOverlay = document.getElementById(this.ELEMENT_OVERLAY);
     var position = this.getPosition(image);
@@ -48,30 +106,24 @@ Clickr.prototype.adjustOverlay = function(image) {
     this.elementOverlay.style.left = position.x + "px";
     this.elementOverlay.style.top = position.y + "px";
     this.elementOverlay.style.width = image.width + "px";
-    this.elementOverlay.style.height = image.height+ "px";
+    this.elementOverlay.style.height = image.height + "px";
 
     this.elementOverlay.innerHTML = "";
 
-
-
     this.tiles = [];
-
-    var sizeX = 50, sizeY = 50,
-        startX = -11, startY = -11,
-        offsetX = sizeX+startX, offsetY = sizeY+startY;
 
     var sizeX = 100, sizeY = 100,
         startX = -21, startY = -21,
-        offsetX = sizeX+startX, offsetY = sizeY+startY;
+        offsetX = sizeX + startX, offsetY = sizeY + startY;
 
-    reqX = Math.ceil( image.width / offsetX );
-    reqY = Math.ceil( image.height / offsetY );
+    reqX = Math.ceil(image.width / offsetX);
+    reqY = Math.ceil(image.height / offsetY);
 
-    this.debug( "width:  " + image.width + " -> " + reqX );
-    this.debug( "height: " + image.height + " -> " + reqY );
+    this.debug("width:  " + image.width + " -> " + reqX);
+    this.debug("height: " + image.height + " -> " + reqY);
 
-    for( var x=0; x<reqX; x++) {
-        for( var y=0; y<reqY; y++) {
+    for (var x = 0; x < reqX; x++) {
+        for (var y = 0; y < reqY; y++) {
 
             var posX = startX + x * offsetX;
             var posY = startY + y * offsetY;
@@ -83,7 +135,7 @@ Clickr.prototype.adjustOverlay = function(image) {
             var tile = document.createElement("div");
             tile.setAttribute("class", "tile");
             tile.setAttribute("style", "left: " + posX + "px; top: " + posY + "px; ");
-            tile.innerHTML = "<img src='" + this.tileDir + "/" + tileSrc +"' width='"+sizeX+"' height='"+sizeY+"' />";
+            tile.innerHTML = "<img src='" + this.tileDir + "/" + tileSrc + "' width='" + sizeX + "' height='" + sizeY + "' />";
             this.elementOverlay.appendChild(tile);
             this.tiles.push(tile);
         }
@@ -93,51 +145,119 @@ Clickr.prototype.adjustOverlay = function(image) {
 
 }
 
+/**
+ * switch to a new image
+ *
+ * Will randomly select one of the remaining images (if any).
+ *
+ * @returns {boolean}
+ */
 Clickr.prototype.newImage = function () {
+
     this.elementImage = document.getElementById(this.ELEMENT_IMAGE);
-    if(this.images.length) {
+
+    if (this.images.length) {
         var idx = Math.floor(Math.random() * this.images.length);
         var imageSrc = this.images[idx];
-        this.elementImage.setAttribute("src", this.imageDir + "/" + imageSrc);
+        // this.elementImage.setAttribute("src", this.imageDir + "/" + imageSrc);
+        this.elementImage.setAttribute("src", imageSrc.src);
         this.images.splice(idx, 1);
         this.adjustOverlay(this.elementImage);
-    }
 
-    return this;
+        return true;
+    }
+    else {
+        return false;
+    }
 
 }
 
-Clickr.prototype.setImageDir = function(directory) {
+/**
+ * Declare the directory holding the images
+ *
+ * @param directory
+ * @returns {Clickr}
+ */
+Clickr.prototype.setImageDir = function (directory) {
     this.imageDir = directory;
     return this;
 }
 
-Clickr.prototype.setTileDir = function(directory) {
+/**
+ * Declare the directory holding the tiles
+ *
+ * @param directory
+ * @returns {Clickr}
+ */
+Clickr.prototype.setTileDir = function (directory) {
     this.tileDir = directory;
     return this;
 }
 
+/**
+ * Remove all remaining tiles, revealing the image
+ *
+ * @returns {Clickr}
+ */
+Clickr.prototype.clear = function () {
+    for (var i = 0; i < this.tiles.length; i++) {
+        this.tiles[i].style.visibility = "hidden";
+    }
+    this.tiles = [];
+    return this;
+}
 
-Clickr.prototype.dropTile = function() {
+/**
+ * Remove a random tile
+ *
+ * @returns {boolean}
+ */
+Clickr.prototype.dropTile = function () {
 
-    if(this.tiles.length) {
+    if (this.tiles.length) {
 
         var idx = Math.floor(Math.random() * this.tiles.length);
-        this.debug( "Remove tile #" + idx);
+        this.debug("Remove tile #" + idx);
         this.tiles[idx].style.visibility = "hidden";
         this.tiles.splice(idx, 1);
 
+        return true;
+    }
+    else {
+        return false;
     }
 
 }
 
-Clickr.prototype.addImage = function(src) {
-    this.images.push(src);
+/**
+ * Add Image to the list of available images.
+ *
+ * Will fail if the image directory was not defined before.
+ *
+ * @param src
+ * @returns {Clickr}
+ */
+Clickr.prototype.addImage = function (src) {
+
+    if (null == this.imageDir) {
+        throw "You have to declare an image directory first";
+    }
+
+    var image = document.createElement("img");
+    image.src = this.imageDir + "/" + src;
+
+
+    this.images.push(image);
     return this;
 }
 
-Clickr.prototype.addTile= function(src) {
-    this.debug("addTile(): " + src);
+/**
+ * Add a tile to the list of available tiles.
+ *
+ * @param src
+ * @returns {Clickr}
+ */
+Clickr.prototype.addTile = function (src) {
     this.tileImages.push(src);
     return this;
 }
--- index.php
+++ index.php
@@ -9,22 +9,29 @@
     <script type="text/javascript">
         var clickr = new Clickr();
         clickr.setImageDir("/image/album").setTileDir("/image/tiles");
-        shortcut.add("n", function() {clickr.newImage()});
-        shortcut.add("c", function() {clickr.dropTile()});
-    <?php
-        $imageDir = $_SERVER['DOCUMENT_ROOT'] . '/image/album';
-        $images = glob($imageDir . '/*');
-        foreach($images as $image) {
-            $image = basename($image);
-            printf( 'clickr.addImage("%s");' . PHP_EOL, $image);
-        }
-        $tileDir = $_SERVER['DOCUMENT_ROOT'] . '/image/tiles';
-        $tiles = glob($tileDir . '/*');
-        foreach($tiles as $tile) {
-            $tile = basename($tile);
-            printf( 'clickr.addTile("%s");' . PHP_EOL, $tile);
-        }
-    ?>
+        shortcut.add("n", function () {
+            clickr.newImage()
+        });
+        shortcut.add("c", function () {
+            clickr.dropTile()
+        });
+        shortcut.add("escape", function () {
+            clickr.clear()
+        });
+        <?php
+            $imageDir = $_SERVER['DOCUMENT_ROOT'] . '/image/album';
+            $images = glob($imageDir . '/*');
+            foreach($images as $image) {
+                $image = basename($image);
+                printf( 'clickr.addImage("%s");' . PHP_EOL, $image);
+            }
+            $tileDir = $_SERVER['DOCUMENT_ROOT'] . '/image/tiles';
+            $tiles = glob($tileDir . '/*');
+            foreach($tiles as $tile) {
+                $tile = basename($tile);
+                printf( 'clickr.addTile("%s");' . PHP_EOL, $tile);
+            }
+        ?>
     </script>
     <style type="text/css">
         div.tile {
@@ -33,32 +40,30 @@
             width: 100px;
             height: 100px;
         }
+
         #overlay {
             position: absolute;
             left: -10000px;
             top: -10000px;
         }
-        #clickr_image {
 
+        #clickr_image {
         }
     </style>
 </head>
 <body>
 
 <h1>Clickr</h1>
-<button onclick="clickr.newImage()">new image</button>
+<button onclick="clickr.newImage() || alert('Keine Bilder mehr!')">new image</button>
 <button onclick="clickr.dropTile()">click</button>
+<button onclick="clickr.clear()">clear</button>
 <br/>
 
-
-
-
 <div style="text-align: center;">
-        <img src="" id="clickr_image" />
-    </div>
+    <img src="" id="clickr_image"/>
+</div>
 
-    <div id="overlay">
-    </div>
+<div id="overlay"></div>
 
 <pre id="debug"></pre>
 
